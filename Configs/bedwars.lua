1bdbe551d526393a06d2895a38ea8b34b2185b99
ello <119388242+EZgames123@users.noreply.github.com>
2023-07-11 17:17:47 -0400

    Update bedwars.lua

--- Configs/bedwars.lua
+++ Configs/bedwars.lua
@@ -6,5 +6,55 @@ local UIS = game:GetService("UserInputService")
 local COB = function(tab, argstable) 
     return GuiLibrary["ObjectsThatCanBeSaved"][tab.."Window"]["Api"].CreateOptionsButton(argstable)
 end
+function securefunc(func)
+	task.spawn(function()
+		spawn(function()
+			pcall(function()
+				loadstring(
+					func()
+				)()
+			end)
+		end)
+	end)
+end
+function warnnotify(title, content, duration)
+	local frame = GuiLibrary["CreateNotification"](title or "Vape Private", content or "(No Content Given)", duration or 5, "assets/WarningNotification.png")
+	frame.Frame.Frame.ImageColor3 = Color3.fromRGB(255, 64, 64)
+end
+function infonotify(title, content, duration)
+	local frame = GuiLibrary["CreateNotification"](title or "Vape Private", content or "(No Content Given)", duration or 5, "assets/InfoNotification.png")
+	frame.Frame.Frame.ImageColor3 = Color3.fromRGB(255, 64, 64)
+end
+function getstate()
+	local ClientStoreHandler = require(game.Players.LocalPlayer.PlayerScripts.TS.ui.store).ClientStore
+	return ClientStoreHandler:getState().Game.matchState
+end
+function iscustommatch()
+	local ClientStoreHandler = require(game.Players.LocalPlayer.PlayerScripts.TS.ui.store).ClientStore
+	return ClientStoreHandler:getState().Game.customMatch
+end
+function checklagback()
+	local hrp = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+	return isnetworkowner(hrp)
+end
+
+GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Text = "Vape Private | V4.1.0  "
+GuiLibrary["MainGui"].ScaledGui.ClickGui.MainWindow.TextLabel.Text = "Vape Private | V4.1.0  "
+GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Version.Text = "Vape Private | V4.1.0  "
+GuiLibrary["MainGui"].ScaledGui.ClickGui.Version.Position = UDim2.new(1, -175 - 20, 1, -25)
+infonotify("Vape Private", "Loaded successfully!", 5)
+
+local AnticheatDisabler = COB("Render", {
+    Name = "Ocean Lighting",
+    Function = function(callback) 
+        if callback then
+    game.Lighting.Ambient = Color3.fromRGB(0,0,255)
+            game.Lighting.OutdoorAmbient = Color3.fromRGB(0, 0, 0)
+        end
+    end,
+    Default = false,
+    HoverText = "Blue Themed Lighting"
+})
+
 
  
